594cca62fccb2c6c410f5a6d2f323298eb7050fe
Nikhil <nikhilgangwar23@gmail.com>
2024-06-08 10:19:56 +1000

    Added dotenv and tests for different test users

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is Webdriver IO Autiomation Demo project.
+This is Webdriver IO Automation Demo project.
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { browser } from '@wdio/globals'
 import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
@@ -6,7 +7,18 @@ import LoginPage from '../pageobjects/login.page.js'
 describe('My Login application', () => {
     it('should be able to login using Standard User', async () => {
         await LoginPage.open()
-        await LoginPage.login('standard_user', 'secret_sauce')                    
+        await LoginPage.login(process.env.STANDARDUSER, process.env.PASSWORD)                    
+    })
+
+    it('should be able to login using performance glitch User', async () => {
+        await LoginPage.open()
+        await LoginPage.login(process.env.PERFORMANCEGLITCHUSER, process.env.PASSWORD)
+                     
+    })
+    it('should not login using invalid user', async () => {
+        await LoginPage.open()        
+        await LoginPage.login('invaliduser', process.env.PASSWORD)
+        await expect(LoginPage.errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service')               
     })
 })
 
