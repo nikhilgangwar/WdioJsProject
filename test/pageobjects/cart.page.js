da672daa967258a1c25ab201b1fa3afbd54b0406
Nikhil <nikhilgangwar23@gmail.com>
2025-10-04 00:02:11 +1000

    Added Comments in Cart Page objects

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -8,17 +8,21 @@ class CartPage extends Page {
     /**
      * define selectors using getter methods
      */
+    //shopping cart link
     get shoppingCartLink() {
          return $('.shopping_cart_link'); 
         }
+    //checkout button    
     get checkoutButton() {
         return $('//button[contains(text(),"Checkout")]'); 
         }  
 
+    // goto shopping cart    
     async goToShoppingCart() {
         await this.shoppingCartLink.click();
     }
 
+    //checkout button
     async proceedToCheckout() {
         await this.checkoutButton.click();
     }
